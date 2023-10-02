16b52d64a4dbad4b354edea484588aad597ef533
Jaiwanth <jaiwanth2011@gmail.com>
2023-10-02 22:21:42 +0530

    feat: add react boilerplate

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import basicSsl from '@vitejs/plugin-basic-ssl';
+import fs from 'fs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), basicSsl()],
-  build: {
-    outDir: './docs'
-  },
-  base: '/vite-boilerplate/'
+  plugins: [react()],
+  server: {
+    port: 443,
+    https: {
+      key: fs.readFileSync('./.cert/key.pem'),
+      cert: fs.readFileSync('./.cert/cert.pem'),
+    }
+  }
 });
