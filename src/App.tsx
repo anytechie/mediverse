854656ef2d21112307a9048a428994d3ac270869
Jaiwanth <jaiwanth2011@gmail.com>
2023-10-10 22:28:57 +0530

    feat: auth

--- src/App.tsx
+++ src/App.tsx
@@ -15,23 +15,26 @@ import BookAppointment from "./components/BookAppointment/BookAppointment";
 import ResolveAppointment from "./components/ResolveAppointment/ResolveAppointment";
 import PastAppointmentDoctor from "./components/PastAppointmentDoctor/PastAppointmentDoctor";
 import ViewAppointment from "./components/ViewAppointmentPatient/PastAppointmentPatient";
+import { AuthProvider } from "./context/AuthContext";
 
 const App = () => {
   const [smoothButtonsTransition, setSmoothButtonsTransition] = useState(false);
   return (
-    <WebAppProvider options={{ smoothButtonsTransition }}>
-        <Routes>
-          <Route path="/" element={<LandingPage onChangeTransition={() => setSmoothButtonsTransition(true)} />} />
-          <Route path="/register_doctor" element={<RegisterDoctor onChangeTransition={() => setSmoothButtonsTransition(true)} />} />
-          <Route path="/register_patient" element={<RegisterPatient onChangeTransition={() => setSmoothButtonsTransition(true)} />} />
-          <Route path="/patient_dashboard" element={<PatientDashboard onChangeTransition={() => setSmoothButtonsTransition(true)} />} />
-          <Route path="/doctor_dashboard" element={<DoctorDashboard onChangeTransition={() => setSmoothButtonsTransition(true)} />} />
-          <Route path="/book_appointment/:doctorId" element={<BookAppointment onChangeTransition={() => setSmoothButtonsTransition(true)} />} />
-          <Route path="/resolve_appointment/:appointmentId" element={<ResolveAppointment onChangeTransition={() => setSmoothButtonsTransition(true)} />} />
-          <Route path="/past_appointment_doctor/:appointmentId" element={<PastAppointmentDoctor onChangeTransition={() => setSmoothButtonsTransition(true)} />} />
-          <Route path="/view_appointment/:appointmentId" element={<ViewAppointment onChangeTransition={() => setSmoothButtonsTransition(true)} />} />
-        </Routes>
-    </WebAppProvider>
+    <AuthProvider>
+      <WebAppProvider options={{ smoothButtonsTransition }}>
+          <Routes>
+            <Route path="/" element={<LandingPage onChangeTransition={() => setSmoothButtonsTransition(true)} />} />
+            <Route path="/register_doctor" element={<RegisterDoctor onChangeTransition={() => setSmoothButtonsTransition(true)} />} />
+            <Route path="/register_patient" element={<RegisterPatient onChangeTransition={() => setSmoothButtonsTransition(true)} />} />
+            <Route path="/patient_dashboard" element={<PatientDashboard onChangeTransition={() => setSmoothButtonsTransition(true)} />} />
+            <Route path="/doctor_dashboard" element={<DoctorDashboard onChangeTransition={() => setSmoothButtonsTransition(true)} />} />
+            <Route path="/book_appointment/:doctorId" element={<BookAppointment onChangeTransition={() => setSmoothButtonsTransition(true)} />} />
+            <Route path="/resolve_appointment/:appointmentId" element={<ResolveAppointment onChangeTransition={() => setSmoothButtonsTransition(true)} />} />
+            <Route path="/past_appointment_doctor/:appointmentId" element={<PastAppointmentDoctor onChangeTransition={() => setSmoothButtonsTransition(true)} />} />
+            <Route path="/view_appointment/:appointmentId" element={<ViewAppointment onChangeTransition={() => setSmoothButtonsTransition(true)} />} />
+          </Routes>
+      </WebAppProvider>
+    </AuthProvider>
   );
 };
 
---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,42 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { getAuth, signInAnonymously, signOut } from "firebase/auth";
+
+export const AuthContext = createContext({
+  currentUser: null,
+});
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
+
+export const AuthProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const auth = getAuth();
+  console.log("auth", auth.currentUser)
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setCurrentUser(user);
+      setLoading(false);
+    });
+
+    // If no user is signed in, sign in anonymously
+    if (!auth.currentUser) {
+      signInAnonymously(auth).catch((error) => {
+        console.error("Error signing in anonymously:", error);
+      });
+    }
+
+    return unsubscribe;
+  }, [auth]);
+
+  const value = {
+    currentUser,
+  };
+
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+};
